a138d42adb28cbad24cd9b73d2a8fcdd23119c7a
wle8300 <email@wle8300.com>
2019-05-16 19:09:32 -0700

    asdf

---
+++ src/index.js
@@ -0,0 +1,62 @@
+export default class Beep {
+  constructor(props) {
+    const url = props.url;
+    const volume = props.volume || 1.0;
+
+    // this is a hack
+    // to force browser to
+    // preload audio file
+    const appendAudioElement = url => {
+      // hash function
+      // credit: https://stackoverflow.com/a/8831937/11330825
+      const hash = str => {
+        var hash = 0;
+        if (str.length === 0) {
+          return hash;
+        }
+        for (var i = 0; i < str.length; i++) {
+          var char = str.charCodeAt(i);
+          hash = (hash << 5) - hash + char;
+          hash = hash & hash; // Convert to 32bit integer
+        }
+        return Math.abs(hash);
+      };
+      const id = `boink-${hash(url)}`;
+
+      let audioElement = document.createElement("audio");
+      audioElement.id = id;
+      audioElement.src = url;
+      audioElement.preload = "auto";
+
+      document.body.appendChild(audioElement);
+      return;
+    };
+
+    // argument validation
+    if (!url) throw Error('Requires valid "url" for audio file');
+    if (volume) {
+      if (typeof volume !== "number" || volume < 0 || volume > 1.0) {
+        throw Error('"Volume" must be a number between 0.0 and 1.0');
+      }
+    }
+
+    appendAudioElement(url);
+
+    this.url = url;
+    this.volume = volume;
+  }
+
+  play = () => {
+    const audioElement = new Audio(this.url);
+
+    audioElement.addEventListener("loadeddata", () => {
+      audioElement.volume = this.volume;
+      audioElement.play();
+    });
+  };
+
+  adjustVolume = volume => {
+    this.volume = volume;
+    return this;
+  };
+}
